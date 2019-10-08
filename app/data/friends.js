a54002f9bdcf6c9de7aaf8a6b95cc4a8be2e9e9c
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-10-08 12:24:17 -0500

    routes, javscript

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,82 @@ var friendsArray = [
     {
         "name": "Oliver",
         "Photo": "https://mymodernmet.com/wp/wp-content/uploads/2019/01/maine-coon-cat-photography-26.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1] 
+        "scores": [
+            5, 
+            1, 
+            4, 
+            4, 
+            5, 
+            1, 
+            2, 
+            5, 
+            4, 
+            1
+        ] 
     },
     {
         "name": "Emmaline",
         "Photo": "https://i2.wp.com/consciouscat.net/wp-content/uploads/2017/09/tabby-cat-2-e1504603272898.jpg",
-        "scores": [2, 4, 2, 1, 4, 3, 5, 1, 1, 4] 
+        "scores": [
+            2, 
+            4, 
+            2, 
+            1, 
+            4, 
+            3, 
+            5, 
+            1, 
+            1, 
+            4
+        ] 
     },
     {
         "name": "Anne",
         "Photo": "https://findinterestingfacts.com/wp-content/uploads/2018/11/cat-1046544_640.jpg",
-        "scores": [3, 2, 1, 2, 3, 2, 4, 2, 2, 3] 
+        "scores": [
+            3, 
+            2, 
+            1, 
+            2, 
+            3, 
+            2, 
+            4, 
+            2, 
+            2, 
+            3
+        ] 
     },
     {
         "name": "Albert",
         "Photo": "https://cdn.images.express.co.uk/img/dynamic/128/590x/secondary/Cat4-430028.jpg",
-        "scores": [5, 5, 2, 1, 3, 2, 4, 3, 3, 2] 
+        "scores": [
+            5, 
+            5, 
+            2, 
+            1, 
+            3, 
+            2, 
+            4, 
+            3, 
+            3, 
+            2
+        ] 
     },
     {
         "name": "Raven",
         "Photo": "https://cdn.cnn.com/cnnnext/dam/assets/141030105303-kiki-irpt-horizontal-large-gallery.jpg",
-        "scores": [1, 2, 3, 5, 2, 4, 5, 4, 5, 5] 
+        "scores": [
+            1, 
+            2, 
+            3, 
+            5, 
+            2, 
+            4, 
+            5, 
+            4, 
+            5, 
+            5
+        ] 
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,9 +8,41 @@ module.exports = function(app) {
     });
 }
 
+function generateDiff(); {
+    
+}
 
+app.post("/../api/friends", function(req,res){
+    var savedName = "";
+    var savedPic = "";
+    var savedDiff = "";
+    for (var i = 0; i < friends.length; i++){
+        var ans = generateDiff();
+        if (ans < savedDiff) {
+            savedDiff = ans;
+            savedPic=person.photo;
+            savedName=person.name;
+        }
+    }
+}
 //POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+//loop through freinds
+
+//math
+
+//store result
+
+//end of loop, array of all results. pick the highest as the best match.
+
+//return that person
+
+
+
+
 
 // app.post("/api/friends", function(req, res){
 
 // })
+
+//res.json(friends);
